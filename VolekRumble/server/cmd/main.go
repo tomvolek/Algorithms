269ff73891c4adff08063f97f312846a207c6c4c
Tom Volek <tomvolek@yahoo.com>
2025-03-01 00:27:28 -0800

    test go

---
+++ VolekRumble/server/cmd/main.go
@@ -0,0 +1,10 @@
+<<<<<<< Tabnine <<<<<<<
+package main
+>>>>>>> Tabnine >>>>>>>// {"conversationId":"dd743743-e8d0-47fb-a19a-ab55f6253334","source":"instruct"}
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello main")
+
+}
